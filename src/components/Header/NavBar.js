d4cfe8c4fcca7e1bcaef8648243c97528337bbd1
Caglar Turali <caglarturali@gmail.com>
2019-01-09 22:19:35 +0300

    Update navbar with icons

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => (
   <nav id="main-nav">
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Skills</a></li>
-      <li><a href="#">Education</a></li>
-      <li><a href="#">Projects</a></li>
-      <li><a href="#">Certifications</a></li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon={['fab', 'html5']} size="sm" color="#e44d26" />
+        <a href="#">home.html</a>
+      </li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon={['fab', 'js']} size="sm" color="#ffca28" />
+        <a href="#">skills.js</a>
+      </li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon="university" size="sm" color="#42a5f5" />
+        <a href="#">.educationrc</a>
+      </li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon="sliders-h" size="sm" color="#98c379" />
+        <a href="#">projects.config</a>
+      </li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon={['fab', 'sass']} size="sm" color="#e13e76" />
+        <a href="#">certifications.sass</a>
+      </li>
+      <li>
+        <FontAwesomeIcon className="main-nav__icon" icon={['fab', 'markdown']} size="sm" color="#abb2bf" />
+        <a href="#">contact.md</a>
+      </li>
     </ul>
   </nav>
 );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee, faSlidersH, faUniversity } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCheckSquare, faCoffee, faSlidersH, faUniversity);
 
 import Header from './Header/Header';
 import Greeter from './Greeter/Greeter';
-
-library.add(fab, faCheckSquare, faCoffee)
 
 const Main = () => (
   <div>
