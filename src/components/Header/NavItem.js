9ff71884752fbd331103a6a862fdf3f7cdbb3989
Caglar Turali <caglarturali@gmail.com>
2019-01-12 02:44:13 +0300

    Make navbar responsive

--- src/components/Header/NavItem.js
+++ src/components/Header/NavItem.js
@@ -5,8 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const NavItem = ({ icon, color, linkTo, linkName }) => (
   <span>
     <Link to={linkTo}>
-      <FontAwesomeIcon className="main-nav__icon" icon={icon} size="sm" color={color} />
-      {linkName}
+      <FontAwesomeIcon className="main-nav__icon" icon={icon} size="lg" color={color} />
+      <span className="main-nav__text">{linkName}</span>
     </Link>
   </span>
 );
