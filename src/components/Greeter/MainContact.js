a69a6fbd2c817c17699fbe79adc5970b1524b1cd
Caglar Turali <caglarturali@gmail.com>
2019-01-12 01:07:14 +0300

    Refactor main contact button

--- src/components/Greeter/MainContact.js
+++ src/components/Greeter/MainContact.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import contactInfo from '../../data/contact.json';
+
+const mainContactPref = contactInfo.filter(contactItem => contactItem.isMain)[0];
 
 const MainContact = () => (
-  <a className="main-button" href="https://t.me/caglarturali" target="_blank" rel="noopener noreferrer">
+  <a 
+    className="main-button" 
+    href={mainContactPref.url} 
+    target="_blank" 
+    rel="noopener noreferrer">
     <span className="main-button__contents">
-      <FontAwesomeIcon className="main-button__icon" icon={['fab', 'telegram-plane']} size="lg" />
+      <FontAwesomeIcon 
+        className="main-button__icon" 
+        icon={mainContactPref.icon} 
+        size="lg" />
       <span className="main-button__text">Contact Me</span>
     </span>
   </a>
