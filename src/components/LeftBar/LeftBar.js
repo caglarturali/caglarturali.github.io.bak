8de3eba3130a3078d2342f21889dd4276f8db869
Caglar Turali <caglarturali@gmail.com>
2019-01-12 01:01:36 +0300

    List contact options on the left bar

--- src/components/LeftBar/LeftBar.js
+++ src/components/LeftBar/LeftBar.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import contactInfo from '../../data/contact.json';
 
 const LeftBar = () => (
   <div id="left-bar">
     <div>
-      <span className="left-bar__icon">
-        <FontAwesomeIcon icon="copy" size="lg" />
-      </span>
-      <span className="left-bar__icon">
-        <FontAwesomeIcon icon="search" size="lg" />
-      </span>
+      {contactInfo.map(contactItem => (
+        <a
+          className="left-bar__item"
+          key={contactItem.name}
+          href={contactItem.url}
+          aria-label={`Find me on ${contactItem.name}`}
+          title={`Find me on ${contactItem.name}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={contactItem.icon} size="lg" />
+        </a>
+      ))}
     </div>
     <div>
-      <span className="left-bar__icon">
+      <span className="left-bar__item" title="Useless button!">
         <FontAwesomeIcon icon="cog" size="lg" />
       </span>
     </div>
