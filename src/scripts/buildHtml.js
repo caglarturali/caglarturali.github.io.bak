761a8b95163f42c837609a889da73bf000e3883e
Caglar Turali <caglarturali@gmail.com>
2019-01-14 05:49:20 +0300

    Overengineer buildHtml script :)

--- src/scripts/buildHtml.js
+++ src/scripts/buildHtml.js
@@ -4,41 +4,37 @@ const rootPath = require('../../root');
 const metadata = require('../data/metadata.json');
 
 const buildMeta = () => {
-  let meta = '';
-  meta += `<title>${metadata.title}</title>`;
-  meta += `<meta name="description" content="${metadata.description}" />`;
-  meta += `<meta name="keywords" content="${metadata.keywords}" />`;
-  meta += `<meta name="author" content="${metadata.author}" />`;
-  meta += `<link rel="canonical" href="${metadata.og.url}" />`;
-  meta += `<meta name="theme-color" content="${metadata.themeColor}" />`;
+  let meta = fs.readFileSync(path.join(__dirname, 'metaTemp.html'), 'utf8');
+  meta = meta.replace('__TITLE__', metadata.title);
+  meta = meta.replace('__DESCRIPTION__', metadata.description);
+  meta = meta.replace('__KEYWORDS__', metadata.keywords);
+  meta = meta.replace('__AUTHOR__', metadata.author);
+  meta = meta.replace('__CANONICAL__', metadata.canonical);
+  meta = meta.replace('__THEME_COLOR__', metadata.themeColor);
 
-  meta += `<!-- Schema.org markup for Google+ -->`;
-  meta += `<meta itemprop="name" content="${metadata.gPlus.name}" />`;
-  meta += `<meta itemprop="description" content="${metadata.gPlus.description}" />`;
-  meta += `<meta itemprop="image" content="${metadata.gPlus.image}" />`;
+  meta = meta.replace('__GPLUS_NAME__', metadata.gPlus.name);
+  meta = meta.replace('__GPLUS_DESCRIPTION__', metadata.gPlus.description);
+  meta = meta.replace('__GPLUS_IMAGE__', metadata.gPlus.image);
 
-  meta += `<!-- Twitter Card data -->`;
-  meta += `<meta name="twitter:card" content="${metadata.twitter.card}" />`;
-  meta += `<meta name="twitter:site" content="${metadata.twitter.site}" />`;
-  meta += `<meta name="twitter:creator" content="${metadata.twitter.creator}" />`;
-  meta += `<meta name="twitter:title" content="${metadata.twitter.title}" />`;
-  meta += `<meta name="twitter:description" content="${metadata.twitter.description}" />`;
-  meta += `<meta name="twitter:image" content="${metadata.twitter.image}" />`;
+  meta = meta.replace('__TWITTER_CARD__', metadata.twitter.card);
+  meta = meta.replace('__TWITTER_SITE__', metadata.twitter.site);
+  meta = meta.replace('__TWITTER_CREATOR__', metadata.twitter.creator);
+  meta = meta.replace('__TWITTER_TITLE__', metadata.twitter.title);
+  meta = meta.replace('__TWITTER_DESCRIPTION__', metadata.twitter.description);
+  meta = meta.replace('__TWITTER_IMAGE__', metadata.twitter.image);
 
-  meta += `<!-- Open Graph data -->`;
-  meta += `<meta property="og:title" content="${metadata.og.title}" />`;
-  meta += `<meta property="og:type" content="${metadata.og.type}" />`;
-  meta += `<meta property="og:url" content="${metadata.og.url}" />`;
-  meta += `<meta property="og:image" content="${metadata.og.image}" />`;
-  meta += `<meta property="og:image:secure_url" content="${metadata.og.image_secure_url}" />`;
-  meta += `<meta property="og:image:width" content="${metadata.og.image_width}" />`;
-  meta += `<meta property="og:image:height" content="${metadata.og.image_height}" />`;
-  meta += `<meta property="og:image:alt" content="${metadata.og.image_alt}" />`;
-  meta += `<meta property="og:description" content="${metadata.og.description}" />`;
-  meta += `<meta property="og:site_name" content="${metadata.og.site_name}" />`;
+  meta = meta.replace('__OG_TITLE__', metadata.og.title);
+  meta = meta.replace('__OG_TYPE__', metadata.og.type);
+  meta = meta.replace('__OG_URL__', metadata.og.url);
+  meta = meta.replace('__OG_IMAGE__', metadata.og.image);
+  meta = meta.replace('__OG_IMAGE_SECURE_URL__', metadata.og.image_secure_url);
+  meta = meta.replace('__OG_IMAGE_WIDTH__', metadata.og.image_width);
+  meta = meta.replace('__OG_IMAGE_HEIGHT__', metadata.og.image_height);
+  meta = meta.replace('__OG_IMAGE_ALT__', metadata.og.image_alt);
+  meta = meta.replace('__OG_DESCRIPTION__', metadata.og.description);
+  meta = meta.replace('__OG_SITE_NAME__', metadata.og.site_name);
 
-  meta += `<!-- Google site verification -->`;
-  meta += `<meta name="google-site-verification" content="${metadata.verification.google}" />`;
+  meta = meta.replace('__VERIFICATION_GOOGLE__', metadata.verification.google);
   return meta;
 };
 
