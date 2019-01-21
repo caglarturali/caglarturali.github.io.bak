782566df501a92051316f3ca98d95605cc7602a8
Caglar Turali <caglarturali@gmail.com>
2019-01-21 07:31:06 +0300

    Keep overengineering :|

--- src/scripts/buildHtml.js
+++ src/scripts/buildHtml.js
@@ -3,38 +3,42 @@ const path = require('path');
 const rootPath = require('../../root');
 const metadata = require('../data/metadata.json');
 
+const metaMap = {
+  __TITLE__: metadata.title,
+  __DESCRIPTION__: metadata.description,
+  __KEYWORDS__: metadata.keywords,
+  __AUTHOR__: metadata.author,
+  __CANONICAL__: metadata.canonical,
+  __THEME_COLOR__: metadata.themeColor,
+  __GPLUS_NAME__: metadata.gPlus.name,
+  __GPLUS_DESCRIPTION__: metadata.gPlus.description,
+  __GPLUS_IMAGE__: metadata.gPlus.image,
+  __TWITTER_CARD__: metadata.twitter.card,
+  __TWITTER_SITE__: metadata.twitter.site,
+  __TWITTER_CREATOR__: metadata.twitter.creator,
+  __TWITTER_TITLE__: metadata.twitter.title,
+  __TWITTER_DESCRIPTION__: metadata.twitter.description,
+  __TWITTER_IMAGE__: metadata.twitter.image,
+  __OG_TITLE__: metadata.og.title,
+  __OG_TYPE__: metadata.og.type,
+  __OG_URL__: metadata.og.url,
+  __OG_IMAGE__: metadata.og.image,
+  __OG_IMAGE_SECURE_URL__: metadata.og.image_secure_url,
+  __OG_IMAGE_WIDTH__: metadata.og.image_width,
+  __OG_IMAGE_HEIGHT__: metadata.og.image_height,
+  __OG_IMAGE_ALT__: metadata.og.image_alt,
+  __OG_DESCRIPTION__: metadata.og.description,
+  __OG_SITE_NAME__: metadata.og.site_name,
+  __VERIFICATION_GOOGLE__: metadata.verification.google
+};
+
 const buildMeta = () => {
   let meta = fs.readFileSync(path.join(__dirname, 'metaTemp.html'), 'utf8');
-  meta = meta.replace('__TITLE__', metadata.title);
-  meta = meta.replace('__DESCRIPTION__', metadata.description);
-  meta = meta.replace('__KEYWORDS__', metadata.keywords);
-  meta = meta.replace('__AUTHOR__', metadata.author);
-  meta = meta.replace('__CANONICAL__', metadata.canonical);
-  meta = meta.replace('__THEME_COLOR__', metadata.themeColor);
 
-  meta = meta.replace('__GPLUS_NAME__', metadata.gPlus.name);
-  meta = meta.replace('__GPLUS_DESCRIPTION__', metadata.gPlus.description);
-  meta = meta.replace('__GPLUS_IMAGE__', metadata.gPlus.image);
+  for (const key in metaMap) {
+    meta = meta.replace(key, metaMap[key]);
+  }
 
-  meta = meta.replace('__TWITTER_CARD__', metadata.twitter.card);
-  meta = meta.replace('__TWITTER_SITE__', metadata.twitter.site);
-  meta = meta.replace('__TWITTER_CREATOR__', metadata.twitter.creator);
-  meta = meta.replace('__TWITTER_TITLE__', metadata.twitter.title);
-  meta = meta.replace('__TWITTER_DESCRIPTION__', metadata.twitter.description);
-  meta = meta.replace('__TWITTER_IMAGE__', metadata.twitter.image);
-
-  meta = meta.replace('__OG_TITLE__', metadata.og.title);
-  meta = meta.replace('__OG_TYPE__', metadata.og.type);
-  meta = meta.replace('__OG_URL__', metadata.og.url);
-  meta = meta.replace('__OG_IMAGE__', metadata.og.image);
-  meta = meta.replace('__OG_IMAGE_SECURE_URL__', metadata.og.image_secure_url);
-  meta = meta.replace('__OG_IMAGE_WIDTH__', metadata.og.image_width);
-  meta = meta.replace('__OG_IMAGE_HEIGHT__', metadata.og.image_height);
-  meta = meta.replace('__OG_IMAGE_ALT__', metadata.og.image_alt);
-  meta = meta.replace('__OG_DESCRIPTION__', metadata.og.description);
-  meta = meta.replace('__OG_SITE_NAME__', metadata.og.site_name);
-
-  meta = meta.replace('__VERIFICATION_GOOGLE__', metadata.verification.google);
   return meta;
 };
 
