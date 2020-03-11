e390cc2cabd377822ea37c4dacf3361c87d35bc8
Caglar Turali <caglarturali@gmail.com>
2020-03-11 21:08:03 +0300

    Define styles for Skills comp

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,24 +2,32 @@
  * Skills component.
  */
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 import Container from '../Container';
 import skills from '../../data/skills.json';
+import styles from './styles';
 
-const Skills = () => (
-  <Container title={skills.sectionTitle}>
-    {skills.content.map((subSection) => (
-      <div key={subSection.title}>
-        <h3>{subSection.title}</h3>
-        <ul>
-          {subSection.items.map((item) => (
-            <li key={item}>
-              <span>{item}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
-  </Container>
-);
+const useStyles = createUseStyles(styles);
+
+const Skills = () => {
+  const classes = useStyles();
+
+  return (
+    <Container title={skills.sectionTitle}>
+      {skills.content.map((subSection) => (
+        <div key={subSection.title}>
+          <h3 className={classes.subSectionTitle}>{subSection.title}</h3>
+          <ul>
+            {subSection.items.map((item) => (
+              <li key={item}>
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default Skills;
---
+++ src/components/Skills/styles.js
@@ -0,0 +1,10 @@
+/**
+ * Skills component styles.
+ */
+import containerStyles from '../Container/styles';
+
+export default () => ({
+  subSectionTitle: {
+    ...containerStyles().subtitle,
+  },
+});
