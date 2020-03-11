00794e8e3c1ba4291f3978d95c019cf7c87cbc3f
Caglar Turali <caglarturali@gmail.com>
2020-03-11 22:54:53 +0300

    BUILD: Wed, 11 Mar 2020 19:54:45 GMT

--- scripts/buildAndCommit.js
+++ scripts/buildAndCommit.js
@@ -3,7 +3,7 @@
  */
 const execSync = require('child_process').execSync;
 const msg =
-  'BUILD: ' + (process.argv[2] ? process.argv[2] : new Date().toTimeString());
+  'BUILD: ' + (process.argv[2] ? process.argv[2] : new Date().toUTCString());
 execSync(`npm run build && git add . && git commit -m "${msg}"`, {
   stdio: [0, 1, 2],
 });
