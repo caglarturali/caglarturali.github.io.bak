7928bb07f0eb076f86bcb6dac2e8639195829807
Caglar Turali <caglarturali@gmail.com>
2020-03-11 14:42:53 +0300

    Convert left bar styles to jss

--- src/components/LeftBar/index.js
+++ src/components/LeftBar/index.js
@@ -2,36 +2,44 @@
  * LeftBar component.
  */
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import contactInfo from '../../data/contact.json';
+import styles from './styles';
 
-const LeftBar = () => (
-  <div id="left-bar">
-    <div>
-      {contactInfo.content.map((contactItem) => (
-        <a
-          className="left-bar__item"
-          key={`left-bar-${contactItem.name}`}
-          href={contactItem.url}
-          aria-label={`Find me on ${contactItem.name}`}
-          title={`Find me on ${contactItem.name}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon
-            className="left-bar__icon"
-            icon={contactItem.icon}
-            size="lg"
-          />
-        </a>
-      ))}
+const useStyles = createUseStyles(styles);
+
+const LeftBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div>
+        {contactInfo.content.map((contactItem) => (
+          <a
+            className={classes.item}
+            key={`left-bar-${contactItem.name}`}
+            href={contactItem.url}
+            aria-label={`Find me on ${contactItem.name}`}
+            title={`Find me on ${contactItem.name}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              className={classes.icon}
+              icon={contactItem.icon}
+              size="lg"
+            />
+          </a>
+        ))}
+      </div>
+      <div>
+        <span className={classes.item} title="Useless button!">
+          <FontAwesomeIcon icon="cog" size="lg" />
+        </span>
+      </div>
     </div>
-    <div>
-      <span className="left-bar__item" title="Useless button!">
-        <FontAwesomeIcon icon="cog" size="lg" />
-      </span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default LeftBar;
