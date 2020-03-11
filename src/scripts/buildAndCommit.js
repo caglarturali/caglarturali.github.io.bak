1b0fbb206118a5f8d2dcfdabb4163fab5128b14b
Caglar Turali <caglarturali@gmail.com>
2020-03-11 22:21:58 +0300

    undefined

---
+++ src/scripts/buildAndCommit.js
@@ -0,0 +1,9 @@
+/**
+ * Based on https://stackoverflow.com/a/55494338
+ */
+const execSync = require('child_process').execSync;
+const msg =
+  'BUILD: ' + process.argv[2] ? process.argv[2] : new Date().toTimeString();
+execSync(`npm run build && git add . && git commit -m "${msg}"`, {
+  stdio: [0, 1, 2],
+});
