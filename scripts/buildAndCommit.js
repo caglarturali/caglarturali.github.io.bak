6f8317cf983e0487587cd90bdbf1193a4ab74a0a
Caglar Turali <caglarturali@gmail.com>
2020-03-12 11:07:33 +0300

    BUILD: Shorten ternary syntax

--- scripts/buildAndCommit.js
+++ scripts/buildAndCommit.js
@@ -2,8 +2,7 @@
  * Based on https://stackoverflow.com/a/55494338
  */
 const execSync = require('child_process').execSync;
-const msg =
-  'BUILD: ' + (process.argv[2] ? process.argv[2] : new Date().toUTCString());
+const msg = 'BUILD: ' + (process.argv[2] || new Date().toUTCString());
 execSync(`npm run build && git add . && git commit -m "${msg}"`, {
   stdio: [0, 1, 2],
 });
