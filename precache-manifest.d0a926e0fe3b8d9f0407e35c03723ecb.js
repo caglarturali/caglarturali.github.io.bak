c1e4668a3ac489447cc1ede151d1c2c152135b92
Travis CI User <travis@example.org>
2020-05-02 15:39:09 +0000

    Updates

---
+++ precache-manifest.d0a926e0fe3b8d9f0407e35c03723ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7cedc70f448e7517f93ef888a8b9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510d3480700d0b881ec1",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "510d3480700d0b881ec1",
+    "url": "/static/js/2.597e56d2.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/static/js/2.597e56d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e32c69b0bdeb1f684c",
+    "url": "/static/js/main.27a829ff.chunk.js"
+  },
+  {
+    "revision": "548aff7b0a1d05807418",
+    "url": "/static/js/runtime-main.d045887c.js"
+  }
+]);
