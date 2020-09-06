9653fffdf9568573ef33263a4e58a42ad020d5b2
Caglar Turali <caglarturali@gmail.com>
2020-09-06 13:16:16 +0300

    Reorder fields

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -34,31 +34,31 @@ export const pages: PageLink[] = [
 
 export const tabs: TabLink[] = [
   {
+    name: 'skills.js',
+    url: '/skills',
     icon: ['fab', 'js'],
     color: jsOrange,
-    url: '/skills',
-    name: 'skills.js',
     mdFileName: 'skills',
   },
   {
+    name: '.educationrc',
+    url: '/education',
     icon: 'university',
     color: midBlue,
-    url: '/education',
-    name: '.educationrc',
     mdFileName: 'education',
   },
   {
+    name: 'projects.config',
+    url: '/projects',
     icon: 'sliders-h',
     color: green,
-    url: '/projects',
-    name: 'projects.config',
     mdFileName: 'projects',
   },
   {
+    name: 'certificates.sass',
+    url: '/certificates',
     icon: ['fab', 'sass'],
     color: pink,
-    url: '/certificates',
-    name: 'certificates.sass',
     mdFileName: 'certificates',
   },
 ];
