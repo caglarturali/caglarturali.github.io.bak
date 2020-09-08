26fdb1ba6035fcd1aa8fd2383302c5a2bd4a7d47
Caglar Turali <caglarturali@gmail.com>
2020-09-08 19:31:43 +0300

    Fix: random UUIDs when testing

--- src/views/__tests__/Diploma.spec.tsx
+++ src/views/__tests__/Diploma.spec.tsx
@@ -4,6 +4,12 @@ import Diploma from '../Diploma';
 import diplomaData from '../../data/json/diploma.json';
 import staticData from '../../data/json/static.json';
 
+// Workaround for react-tooltip's randomly generated UUIDs.
+// https://github.com/wwayne/react-tooltip/issues/562
+jest.mock('crypto', () => ({
+  randomBytes: (num: number) => new Array(num).fill(0),
+}));
+
 describe('<Diploma />', () => {
   test('should render correctly', () => {
     const { asFragment } = render(
