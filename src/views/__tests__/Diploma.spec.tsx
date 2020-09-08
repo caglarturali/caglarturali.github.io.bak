364157687b94fefd01c8dd64ce52a0777eaa9512
Caglar Turali <caglarturali@gmail.com>
2020-09-08 17:59:18 +0300

    Fix: test desc

--- src/views/__tests__/Diploma.spec.tsx
+++ src/views/__tests__/Diploma.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Diploma from '../Diploma';
 import diplomaData from '../../data/json/diploma.json';
 
-describe('<Greeter />', () => {
+describe('<Diploma />', () => {
   test('should render correctly', () => {
     const { asFragment } = render(<Diploma diplomaData={diplomaData} />);
     expect(asFragment()).toMatchSnapshot();
