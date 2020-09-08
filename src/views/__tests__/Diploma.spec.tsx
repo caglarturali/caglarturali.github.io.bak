3d08d9586c0e37d02d5b5838a61ac28c0e1f9ec2
Caglar Turali <caglarturali@gmail.com>
2020-09-08 19:05:14 +0300

    Fix: tests

--- src/views/__tests__/Diploma.spec.tsx
+++ src/views/__tests__/Diploma.spec.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Diploma from '../Diploma';
 import diplomaData from '../../data/json/diploma.json';
+import staticData from '../../data/json/static.json';
 
 describe('<Diploma />', () => {
   test('should render correctly', () => {
-    const { asFragment } = render(<Diploma diplomaData={diplomaData} />);
+    const { asFragment } = render(
+      <Diploma diplomaData={diplomaData} staticData={staticData} />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
