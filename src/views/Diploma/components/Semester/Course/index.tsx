5069b4dced7524e7888abc53e60f1e84a6da52ee
Çağlar Turalı <caglarturali@gmail.com>
2020-10-09 00:43:52 +0300

    Use max value as the course progress - clean up the naming of the vars

--- src/views/Diploma/components/Semester/Course/index.tsx
+++ src/views/Diploma/components/Semester/Course/index.tsx
@@ -18,38 +18,32 @@ const Course: React.FC<CourseProps> = ({ courseData, showDetails = false }) => {
   const classes = useStyles();
   const { courseName, books } = courseData;
 
-  const [courseProgress, setCourseProgress] = useState<DiplomaTypes.Progress[]>(
+  const [bookProgresses, setBookProgresses] = useState<DiplomaTypes.Progress[]>(
     [],
   );
 
-  const handleCourseProgressChange = (progress: DiplomaTypes.Progress) => {
-    setCourseProgress([...courseProgress, progress]);
+  const handleBookProgressChange = (progress: DiplomaTypes.Progress) => {
+    setBookProgresses([...bookProgresses, progress]);
   };
 
-  const courseProgressAverage = useMemo(() => {
-    if (courseProgress.length === 0) return 0;
-
-    const totalProgress = courseProgress.reduce(
-      (prev, current) => prev + current.progress,
-      0,
-    );
-    return totalProgress === 0
-      ? 0
-      : parseFloat((totalProgress / courseProgress.length).toFixed(1));
-  }, [courseProgress]);
+  // Use the max value as the course progress!
+  const courseProgress = useMemo(() => {
+    if (bookProgresses.length === 0) return 0;
+    return Math.max(...bookProgresses.map((p) => p.progress));
+  }, [bookProgresses]);
 
   return (
     <details open={showDetails} className={classes.courseDetails}>
       <summary>
         <span>{courseName}</span>
-        <ProgressBar value={courseProgressAverage} />
+        <ProgressBar value={courseProgress} />
       </summary>
       <div className={classes.bookPanel}>
         {books.map((book) => (
           <Book
             key={book.isbn[0]}
             courseBook={book}
-            onProgressChange={handleCourseProgressChange}
+            onProgressChange={handleBookProgressChange}
           />
         ))}
       </div>
