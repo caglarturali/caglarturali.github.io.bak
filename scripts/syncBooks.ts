52f03c6f784c0104d49c8850636a62453eb62530
Çağlar Turalı <caglarturali@gmail.com>
2020-10-08 18:06:08 +0300

    Finalize script and books data - for now :blush:

--- scripts/syncBooks.ts
+++ scripts/syncBooks.ts
@@ -8,8 +8,20 @@ import diplomaData from '../src/data/json/diploma.json';
 const DATA_DIR = path.join(__dirname, '..', 'src', 'data', 'json');
 const BOOKS_FILE_PATH = path.join(DATA_DIR, 'books.json');
 
+const STATIC_DATA: DiplomaTypes.BookMetadata[] = [
+  {
+    isbn: ['0769551661', '9780769551661'],
+    title:
+      'Guide to the Software Engineering Body of Knowledge (SWEBOK®): Version 3.0',
+    thumbnail:
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1422647423l/20665102.jpg',
+    link:
+      'https://www.goodreads.com/book/show/20665102-guide-to-the-software-engineering-body-of-knowledge-swebok-r',
+  },
+];
+
 const collectBooks = (curriculum: DiplomaTypes.Curriculum) => {
-  const books: DiplomaTypes.BookEntry[] = [];
+  const books: DiplomaTypes.CourseBook[] = [];
   const { terms, extras } = curriculum;
 
   // Main books
@@ -36,21 +48,23 @@ const process = async (
   const booksData: DiplomaTypes.BookMetadata[] = [];
 
   const promises = books.map(async (book) => {
-    const {
-      isbn: [, isbn13],
-      static: staticData,
-    } = book;
+    const { isbn } = book;
+    const [isbn10, isbn13] = isbn;
+
+    const staticData = STATIC_DATA.find(
+      (s) => s.isbn.includes(isbn10) || s.isbn.includes(isbn13),
+    );
 
     if (staticData) {
       return {
-        isbn: isbn13,
+        isbn,
         staticData,
       };
     }
 
     const data = await getDetailsForIsbn(isbn13);
     return {
-      isbn: isbn13,
+      isbn,
       data,
     };
   });
@@ -62,7 +76,7 @@ const process = async (
 
       if (staticData) {
         booksData.push({
-          ...(staticData as DiplomaTypes.BookMetadata),
+          ...staticData,
           isbn,
         });
       } else if (data) {
@@ -88,7 +102,14 @@ const process = async (
   } catch (error) {
     console.log(error);
   } finally {
-    // Write to file
+    // Sort books and write to file
+    booksData.sort((a, b) => {
+      const titleA = a.title.toLowerCase();
+      const titleB = b.title.toLowerCase();
+      if (titleA < titleB) return -1;
+      if (titleA > titleB) return 1;
+      return 0;
+    });
     fs.writeFile(filePath, JSON.stringify(booksData, null, 2));
   }
 };
