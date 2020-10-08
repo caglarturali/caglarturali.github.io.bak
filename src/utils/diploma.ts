a47a1efca6959f28efc526c1111168773fdbcdb6
Çağlar Turalı <caglarturali@gmail.com>
2020-10-08 20:08:30 +0300

    Implement helper methods

--- src/utils/diploma.ts
+++ src/utils/diploma.ts
@@ -1,8 +1,46 @@
-import { DiplomaTypes } from '../models';
+import { DiplomaTypes, LocalCache } from '../models';
+
+export const collectBooks = (curriculum: DiplomaTypes.Curriculum) => {
+  const books: DiplomaTypes.CourseBook[] = [];
+  const { terms, extras } = curriculum;
+
+  // Main books
+  for (const term of terms) {
+    const { courses } = term;
+    for (const course of courses) {
+      books.push(...course.books);
+    }
+  }
+
+  // Extras
+  for (const extra of extras) {
+    books.push(...extra.books);
+  }
+
+  return books;
+};
+
+export const createBookRecordKey = (isbn: string[]) => {
+  const [, isbn13] = isbn;
+  return `${isbn13}-progress`;
+};
+
+export const getBookProgressFromLocalStorage = (isbn: string[]) => {
+  try {
+    const record = JSON.parse(
+      window.localStorage.getItem(createBookRecordKey(isbn)) || '',
+    ) as LocalCache.Record<number>;
+    return record.data || 0;
+  } catch (error) {
+    return 0;
+  }
+};
 
 export const calculateCourseProgress = (course: DiplomaTypes.Course) => {
   const { books } = course;
-  const highest = Math.max(...books.map((b) => b.progress));
+  const highest = Math.max(
+    ...books.map((b) => getBookProgressFromLocalStorage(b.isbn)),
+  );
   return Math.min(highest, 100);
 };
 
